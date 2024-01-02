67aef917f96114cc59cf75e984f0e51f881f7656
manishv9669 <manishverma1951956@gmail.com>
2024-01-02 23:00:56 +0530

    New file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const account_Id = 4435
+let account_Email = "froggy@google.com"
+var account_Password = "9669"
+account_City = "Ranchi"
+let account_State;
+
+console.log(account_Id);
+console.table([account_Id, account_Email, account_Password, account_City, account_State])
