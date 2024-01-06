90909e9653e761e39b72a0b501e2df59e4fdc81b
manishv9669 <manishverma1951956@gmail.com>
2024-01-06 12:29:42 +0530

    Operations

--- 01_basics/03_conversionOperaion.js
+++ 01_basics/03_conversionOperaion.js
@@ -12,6 +12,35 @@ let booleanLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// ****************** Operation ************************
+
+let value  = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello";
+let str2 = " John";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+let gameCounter = 100;
+gameCounter++
+// console.log(gameCounter);
+
+let x = 3;
+let y = x++ + x++;
+console.log(x , y);
+
+let a = 3;
+let z = ++a + ++a;
+console.log(a , z);
