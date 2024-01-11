2a41aa75ccf73be264d39ae70585da7b56db6a83
manishv9669 <manishverma1951956@gmail.com>
2024-01-11 12:28:49 +0530

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,12 @@
+const name = "John";
+const repoCount = "55";
+
+// console.log(`My name is ${name} and i have total ${repoCount} repos`);
+
+const gameName = new String("Hello")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('l'));
